a30fc3c1ce71c9835f99d773b125aaa6b3fd9b1e
shlainn <shlainnblaze@gmail.com>
2012-10-14 23:04:00 +0200

    Fix Quest 11129 - Kyle's Gone Missing!

--- _develope/1.1.0/shlainn.sql
+++ _develope/1.1.0/shlainn.sql
@@ -4,3 +4,9 @@
 -- Note: UDB has unlimited source items.
 
 UPDATE quest_template SET ReqSourceCount1 = '5' WHERE entry =746;
+
+-- Fix Quest 11129 Kyle's Gone Missing!
+-- Bug: Did not drop the Source Item "Tender Strider Meat" (33009).
+-- Fix: Add Source Item 33009. From UDB
+
+UPDATE quest_template SET ReqSourceId1 = '33009' WHERE entry =11129;
