e4e3254f750ff6a4b5123a8bb3af265771438a8f
Salja <salja2012@hotmail.de>
2012-10-14 13:05:29 +0200

    Some Updates for Sunwell ScriptDevOne Commit [s2632] Thanks Xfurry
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develope/1.1.0/salja.sql
+++ _develope/1.1.0/salja.sql
@@ -220,3 +220,51 @@ INSERT INTO creature (guid,id,map,spawnMask,modelid,equipment_id,position_x,posi
 --
 -- End Some Updates for Magtheridon by ScriptDevOne Commit [s2629] Thanks Xfurry
 --
+
+--
+-- Start Some Updates for Sunwell ScriptDevOne Commit [s2632] Thanks Xfurry
+--
+
+UPDATE gameobject_template SET flags=flags|16 WHERE entry IN (187869, 188114, 188115, 188116);
+
+DELETE FROM gameobject WHERE id IN (187869, 188114, 188115, 188116);
+INSERT INTO gameobject (guid,id,map,spawnMask,position_x,position_y,position_z,orientation,rotation0,rotation1,rotation2,rotation3,spawntimesecs,animprogress,state) VALUES
+(80004, 188116, 580, 1, 1703.91, 581.981, 28.132, 1.761, 0, 0, 0.771057, 0.636766, 180, 0, 1),
+(80005, 188115, 580, 1, 1747.59, 621.556, 28.05, 3.111, 0, 0, 0.999883, 0.0152957, 180, 0, 1),
+(80006, 188114, 580, 1, 1694.32, 676.565, 28.05, 4.808, 0, 0, 0.672508, -0.74009, 180, 0, 1),
+(80007, 187869, 580, 1, 1651.39, 635.547, 28.142, 6.163, 0, 0, 0.0600564, -0.998195, 180, 0, 1);
+
+DELETE FROM creature WHERE id=25640;
+INSERT INTO creature (guid,id,map,spawnMask,modelid,equipment_id,position_x,position_y,position_z,orientation,spawntimesecs,spawndist,currentwaypoint,curhealth,curmana,DeathState,MovementType) VALUES
+(99656, 25640, 580, 1, 0, 0, 1703.91, 581.981, 28.132, 1.761, 300, 0, 0, 1, 0, 0, 0),
+(99657, 25640, 580, 1, 0, 0, 1747.59, 621.556, 28.05, 3.111, 300, 0, 0, 1, 0, 0, 0),
+(99658, 25640, 580, 1, 0, 0, 1694.32, 676.565, 28.05, 4.808, 300, 0, 0, 1, 0, 0, 0),
+(99659, 25640, 580, 1, 0, 0, 1651.39, 635.547, 28.142, 6.163, 300, 0, 0, 1, 0, 0, 0);
+
+UPDATE creature_template SET unit_flags=unit_flags|33554432 WHERE entry=25640;
+UPDATE creature_template SET InhabitType=InhabitType|4 WHERE entry IN (25319,26046,25502);
+
+DELETE FROM creature_linking_template WHERE entry IN (26046, 25319, 25502, 25708,25588, 25735);
+INSERT INTO creature_linking_template (entry, map, master_entry, flag, search_range) VALUES
+(26046, 580, 25315, 4, 0),
+(25319, 580, 25315, 4096, 0),
+(25502, 580, 25315, 4112, 0),
+(25708, 580, 25315, 4112, 0),
+(25735, 580, 25315, 4112, 0),
+(25588, 580, 25588, 15, 0);
+
+DELETE FROM creature WHERE id=26046;
+INSERT INTO creature (guid,id,map,spawnMask,modelid,equipment_id,position_x,position_y,position_z,orientation,spawntimesecs,spawndist,currentwaypoint,curhealth,curmana,DeathState,MovementType) VALUES
+(99660, 26046, 580, 1, 0, 0, 1698.45, 628.03, 50.3889, 0.331613, 604800, 0, 0, 1000, 10000, 0, 0);
+
+UPDATE creature_template SET minhealth=1000, maxhealth=1000, minmana=10000, maxmana=10000 WHERE entry=26046;
+
+UPDATE creature_template SET minlevel=70, maxlevel=70, minhealth=13986, maxhealth=13986, armor=6740, faction_A=14, faction_H=14 WHERE `entry` = 25502;
+
+UPDATE creature_template SET spell1=45862, spell2=45860, spell3=45856, spell4=45848 WHERE entry=25653;
+
+UPDATE creature_template SET minhealth=9347800, maxhealth=9347800, minmana=1693500, maxmana=1693500 WHERE entry=25315;
+
+--
+-- End Some Updates for Sunwell ScriptDevOne Commit [s2632] Thanks Xfurry
+--
