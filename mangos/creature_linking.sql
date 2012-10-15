eecc264c71259d129237d6057b02078d2328183c
Salja <salja2012@hotmail.de>
2012-10-15 19:57:45 +0200

    Creature Movement for some Creatures Thanks UDB 15
    Signed-off-by: Salja <salja2012@hotmail.de>

--- mangos/creature_linking.sql
+++ mangos/creature_linking.sql
@@ -50,7 +50,10 @@ LOCK TABLES `creature_linking` WRITE;
 /*!40000 ALTER TABLE `creature_linking` DISABLE KEYS */;
 INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES
 (57507,57506,515),
-(57527,57528,515);
+(57527,57528,515),
+(57594,57595,515),
+(57595,57596,515),
+(57596,57597,515);
 /*!40000 ALTER TABLE `creature_linking` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
